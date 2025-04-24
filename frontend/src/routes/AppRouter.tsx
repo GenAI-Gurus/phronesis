0eb5b28c805341026dc421a659b072bf31e605a7
Carlos Hernandez-Vaquero <carloshvp@gmail.com>
2025-04-24 15:06:51 +0200

    feat: add /chat route for DecisionSupportChatPage to enable E2E/system chat tests and user access

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
 import DashboardPage from '../pages/DashboardPage';
+import DecisionSupportChatPage from '../pages/DecisionSupportChatPage';
 import NewDecisionPage from '../pages/NewDecisionPage';
 import JournalListPage from '../pages/JournalListPage';
 import JournalDetailPage from '../pages/JournalDetailPage';
@@ -26,6 +27,7 @@ const AppRouter: React.FC = () => (
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/chat" element={<DecisionSupportChatPage />} />
       <Route path="/journal" element={<JournalListPage />} />
       <Route path="/journal/new" element={<NewDecisionPage />} />
       <Route path="/journal/:id" element={<JournalDetailPage />} />
